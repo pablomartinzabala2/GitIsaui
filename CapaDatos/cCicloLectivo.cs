a32342ea016d0f7a0119fe067e3713f34bbffd34
maxichanan <54635577+maxichanan@users.noreply.github.com>
2020-06-12 11:29:30 -0300

    Ciclo Lectivo
    subo la clases y frm de ciclo lectivo

---
+++ CapaDatos/cCicloLectivo.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CapaDatos
+{
+    public class cCicloLectivo
+    {
+        // CodCiclo   Nombre(varchar 20)   Activo(bit)
+
+        //public void AddCicloLectivo(string fecha)
+        //{
+        //    string con = $"insert into CicloLectivo (Nombre, Activo) values ('{ fecha}' )";
+        //    cDb.Grabar(con);
+        //}
+        
+        //public void ValidateYear()
+        //{
+        //    string cad = $"select Nombre from CicloLectivo";
+        //    cDb.GetDatos(cad);
+        //    int ano = DateTime.Now.Year;
+        //    int anoBD = int.Parse(cad);
+        //    string con="";
+        //    if (anoBD < ano)
+        //    {
+        //         con = "update CicloLectivo set Activo=0";
+        //    }
+        //    if (anoBD==ano)
+        //    {
+        //        con = "update CicloLectivo set Activo=1";
+        //    }
+        //    else
+        //    {
+        //        con = "update CicloLectivo set Activo=null";
+        //    }
+        //    cDb.Grabar(con);
+        //}
+    }
+}
---
+++ Isaui/FrmCicloLectivo.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="FrmCicloLectivo.aspx.cs" Inherits="FrmCicloLectivo" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server"> 
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    <br />
+<div >
+    <div><label>Selecciones el Ciclo</label> <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList></div>
+    <button class="btn-primary ">Comenzar ciclo lectivo</button>
+    <br />
+</div>
+
+    <div >
+    <div><label>Crear Ciclo Lectivo</label> <input type="text" id="txtNFecha" placeholder="Ej: 2020"  class="text-center form-control" /></div>
+        <asp:Button ID="BtnCrearCLectivo" runat="server" Text="Crear ciclo lectivo" class="btn-primary " OnClick="BtnCCL"/>
+        
+    <br />
+</div>
+</asp:Content>
+
+
